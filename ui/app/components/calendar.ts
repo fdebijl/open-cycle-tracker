3e21927650f5435b55da72c5ebe71509c4f4c876
Floris de Bijl <floris@feedbackfruits.com>
2023-12-17 23:05:31 +0100

    feat: link calendar cells to day route

---
+++ ui/app/components/calendar.ts
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
+
+type CalendarArgs = {
+  // TBD
+};
+
+export default class Calendar extends Component<CalendarArgs> {
+}
